a7474a75438eee90eda74ace776a7f5455dace55
Shiyu Wang <shiyu3169@gmail.com>
2019-07-15 15:06:30 -0400

    search

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>{{ $route.params.id}}</div>
+  <div>
+    <nuxt-link to="/jokes">Back To jokes</nuxt-link>
+    <h2>{{ joke }}</h2>
+    <hr />
+    <small>joke Id: {{ $route.params.id }}</small>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      joke: {}
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+      this.joke = res.data.joke;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
