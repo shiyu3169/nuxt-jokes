360edf7a7888aca1485a224036758c0a4ef718e7
Shiyu Wang <shiyu3169@gmail.com>
2019-07-15 14:13:05 -0400

    get jokes from api

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,9 +1,47 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import Joke from "../../components/Joke";
+export default {
+  components: {
+    Joke
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
